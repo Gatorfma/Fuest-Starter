95fca2e0842f20a3d5dbc368e406035bbfc08841
Gatorfma <mert.aksakal@ug.bilkent.edu.tr>
2024-12-24 14:47:10 +0300

    Hot fix

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,10 +1,8 @@
 import "server-only";
-
-import { createHydrationHelpers } from "@trpc/react-query/rsc";
 import { headers } from "next/headers";
 import { cache } from "react";
 
-import { createCaller, type AppRouter } from "~/server/api/root";
+
 import { createTRPCContext } from "~/server/api/trpc";
 import { createQueryClient } from "./query-client";
 
@@ -22,9 +20,3 @@ const createContext = cache(() => {
 });
 
 const getQueryClient = cache(createQueryClient);
-const caller = createCaller(createContext);
-
-export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
-  caller,
-  getQueryClient
-);
