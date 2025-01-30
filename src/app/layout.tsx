00a49e3b7a467b07670fe0afec3ec09ad8e439dd
Gatorfma <mert.aksakal@ug.bilkent.edu.tr>
2025-01-30 10:43:31 +0300

    Restart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,19 @@
 import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { WagmiProvider, createConfig, http } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
+import { mainnet, sepolia } from 'wagmi/chains'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { TRPCReactProvider } from "~/trpc/react";
 import { AuthProvider } from './_components/AuthContext';
 
 const config = createConfig({
-  chains: [mainnet],
+  chains: [mainnet, sepolia],
+  multiInjectedProviderDiscovery: false,
   transports: {
-    [mainnet.id]: http()
-  }
-});
+    [mainnet.id]: http('https://mainnet.example.com'),
+    [sepolia.id]: http('https://sepolia.example.com'),
+  },
+})
 
 const queryClient = new QueryClient();
 
